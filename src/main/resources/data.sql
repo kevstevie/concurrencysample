edab5eb26fad7f2f4e7bc7ec31ae15824ef57871
kevstevie <jianna442@gmail.com>
2024-09-20 18:08:19 +0900

    test: 동시성 검증

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -3,3 +3,6 @@ create table money
     id    bigint primary key auto_increment,
     money bigint
 );
+
+insert into money (money)
+values (0);
--- src/test/kotlin/com/sample/concurrency/ConcurrencyTest.kt
+++ src/test/kotlin/com/sample/concurrency/ConcurrencyTest.kt
@@ -1,11 +1,41 @@
 package com.sample.concurrency
 
-import org.junit.jupiter.api.Test
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.RepeatedTest
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import java.util.concurrent.CountDownLatch
+import java.util.concurrent.Executors
 
-class ConcurrencyTest {
+@SpringBootTest
+class ConcurrencyTest(
+    @Autowired private val depositService: DepositService,
+    @Autowired private val moneyRepository: MoneyRepository
+) {
 
-    @Test
-    fun a() {
-        Thread { }
+    @RepeatedTest(100)
+    fun 동시_입금시_실패하지_않음() {
+        val count = 5
+        val threadPool = Executors.newFixedThreadPool(count)
+        val latch = CountDownLatch(count)
+        val money = moneyRepository.save(Money(money = 0))
+
+        for (i in 1..count) {
+            try {
+                threadPool.execute {
+                    depositService.update(money.id, 100)
+                }
+            } catch (e: Exception) {
+
+            } finally {
+                latch.countDown()
+            }
+        }
+
+        latch.await()
+        Thread.sleep(500)
+        threadPool.shutdown()
+        val updatedMoney = moneyRepository.findById(money.id)
+        assertThat(updatedMoney.money).isEqualTo(100L * count)
     }
 }
